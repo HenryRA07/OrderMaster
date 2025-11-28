a111195406a7ad0168c17e7421537c14cdfee546
Alexander_Gallo <rodolfo.gallo@unl.edu.ec>
2025-11-28 04:36:44 -0500

    feat: Test agregar items de la clase Pedido

--- src/test/java/edu/unl/cc/ordermaster/domain/PedidoTest.java
+++ src/test/java/edu/unl/cc/ordermaster/domain/PedidoTest.java
@@ -6,52 +6,57 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class PedidoTest {
-    private Menu Menu1;
+
+    private Menu menu1;
     private Cliente clienteTest, clienteTest2;
     private Producto p1, p2, b1, b2;
     private ItemMenu item1, item2, item3, item4;
-    private ItemPedido linea1,  linea2, linea3, linea4, linea5;
+    private ItemPedido linea1, linea2, linea3, linea4, linea5;
     private Pedido pedidoTest, pedidoTest2;
+
     @BeforeEach
     void setUp() {
-        Menu1 = new Menu("MenuLunes", TipoMenu.ALMUERZO);
+
+        menu1 = new Menu("MenuLunes", TipoMenu.ALMUERZO);
         clienteTest = new Cliente("Juan", "Pérez", "03243202", "098789458", "email.google.com");
-        p1 = new Platillo("Tacos de Barbacoa", "Tacos de borrego cocido en penca con salsa de chiles secos y cebolla encurtida");
-        p2 = new Platillo("Ensalada de atún", "Tacos de borrego cocido en penca con salsa de chiles secos y cebolla encurtida");
+        clienteTest2 = new Cliente("Maria", "Lopez", "03243203", "098123456", "maria@test.com");
 
-        b1 = new Bebida("Brisa Andina", "Refresco natural a base de hierbaluisa y menta");
-        b2 = new Bebida("Cacao Nocturno", "Leche vegetal especiada infusionada con cacao orgánico");
-
+        p1 = new Platillo("Tacos de Barbacoa", "Descripción");
+        p2 = new Platillo("Ensalada de atún", "Descripción");
+        b1 = new Bebida("Brisa Andina", "Descripción");
+        b2 = new Bebida("Cacao Nocturno", "Descripción");
 
         item1 = new ItemMenu(10.50f, true, p1);
         item2 = new ItemMenu(14.50f, true, p2);
         item3 = new ItemMenu(10.50f, true, b1);
         item4 = new ItemMenu(4.70f, true, b2);
 
-
-        linea1 = new ItemPedido(2,item1);
-        linea2 = new ItemPedido(3,item2);
-        linea3 = new ItemPedido(1,item3);
-        linea4 = new ItemPedido(2,item4);
-        linea5 = new ItemPedido(3,item2);
+        linea1 = new ItemPedido(2, item1);
+        linea2 = new ItemPedido(3, item2);
+        linea3 = new ItemPedido(1, item3);
+        linea4 = new ItemPedido(2, item4);
+        linea5 = new ItemPedido(3, item2);
 
         pedidoTest = new Pedido(4, "Hola", clienteTest);
         pedidoTest2 = new Pedido(5, "Hola", clienteTest2);
 
-    }
+        item3 = new ItemMenu(12.00f, true, b1);
+        linea3 = new ItemPedido(1, item3);  // 12.00
 
+        pedidoTest.agregarItems(linea1, linea2, linea3);
+    }
 
     @Test
     void agregarItems() {
 
-        assertEquals(2, pedidoTest.getItemPedido().size());
+        assertEquals(3, pedidoTest.getItemPedido().size());
         assertEquals(76.50f, pedidoTest.getPrecioTotal(), 0.001f);
         pedidoTest2.agregarItem(linea5);
-        assertEquals(33.90f, pedidoTest2.getPrecioTotal(), 0.001f);
+        assertEquals(43.50f, pedidoTest2.getPrecioTotal(), 0.001f);
     }
 
     @Test
     void eliminarItem() {
-
+        //
     }
 }
